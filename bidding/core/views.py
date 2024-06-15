621e8ce5b73f14e3f923f78521e48197793c1ee0
Varad J <varad.gcs@gmail.com>
2024-06-16 02:08:27 +0530

    Define future views

--- bidding/core/views.py
+++ bidding/core/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "core/index.html")
+
+def addBid(request):
+
+def removeBid(request):
+
+def addProduct(request):
+
+def removeProduct(request):
+
+def showBids(request):
